600f5b60374e0f69acb1d2750c1a4b66da237cbd
elis-wind <nbobkova174@gmail.com>
2022-07-22 23:48:09 +0200

    add openning in google search option

--- Data_constitution/open_url.py
+++ Data_constitution/open_url.py
@@ -12,5 +12,10 @@ for word in words:
     segment = request.quote(word.encode('cp1251'))
         
     url1 = "https://search1.ruscorpora.ru/search.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&mydocsize=&spd=&text=lexgramm&mode=main&sort=gr_tagging&lang=ru&nodia=1&parent1=0&level1=0&lex1="+segment+"&gramm1=&sem1=&sem-mod1=sem&sem-mod1=sem2&flags1=&m1=&parent2=0&level2=0&min2=1&max2=1&lex2=&gramm2=&sem2=&sem-mod2=sem&sem-mod2=sem2&flags2=&m2=&p=0"
+
+    url2 = "https://www.google.com/search?channel=fs&client=ubuntu&q="+word
+
     webbrowser.open(url1, new=2)
+    webbrowser.open(url2, new=2)
+
     time.sleep(2) #prevent "429 Too Many Requests" error
